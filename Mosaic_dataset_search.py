7205ab2c2ce2d0246eedf617113ff723924b0559
Gombos Levente <glevi91@hotmail.com>
2016-09-06 20:28:52 +0200

    Update Mosaic_dataset_search.py
    Clarified variable names and paths.

--- Mosaic_dataset_search.py
+++ Mosaic_dataset_search.py
@@ -5,26 +5,25 @@ reload (sys)
 sys.setdefaultencoding('utf-8')
 
 inputRaster = sys.argv[1] # Select raster
-kulhattext = sys.argv[2] # Select "kulhat"
+landtext = sys.argv[2] # Select "land"
 
 scriptPath = sys.path[0]
 toolSharePath = os.path.dirname(scriptPath)
 toolDataPath = os.path.join(toolSharePath)
-kulhatfeatureEOV = os.path.join(toolDataPath,"KULTHAT.SHP")
-kulhatfeatureUTM = os.path.join(toolDataPath,"KULHAT_UTM.SHP")
-#arcpy.env.workspace = Balaton_raszteres_adatok.gdb" # Setting default geodatabase
+landfeatureEOV = os.path.join(toolDataPath,"dummy.SHP") #EOV projection
+landfeatureUTM = os.path.join(toolDataPath,"dummy_UTM.SHP") #UTM projection
 arcpy.env.overwriteOutput = True
 
 
 inputs = arcpy.GetParameterAsText(0) # Getting input mosaic dataset parameters as text
 
 
-for row in arcpy.SearchCursor(kulhatfeatureEOV, fields="NEV"):
-   nevvalue=row.getValue("NEV")
-   if nevvalue == kulhattext:
+for row in arcpy.SearchCursor(landfeatureEOV, fields="NAME"):
+   namevalue=row.getValue("NAME")
+   if namevalue == landtext:
       break
 
-query = """ "NEV" = '%s'"""%nevvalue
+query = """ "NAME" = '%s'"""%namevalue
 
 	  
 for dataset in inputs.split(';'):
@@ -37,16 +36,16 @@ for dataset in inputs.split(';'):
          arcpy.DeleteField_management(dataset,"Path")
    
    if wkid == 23700:
-      arcpy.MakeFeatureLayer_management(kulhatfeatureEOV,"kulhatfeature_lyr_EOV",query)
-      arcpy.CopyFeatures_management("kulhatfeature_lyr_EOV","R:\Kereso_allomanyai\kulhatlayer_EOV.SHP")
-      EOV = os.path.join("R:\Kereso_allomanyai\kulhatlayer_EOV.SHP")
+      arcpy.MakeFeatureLayer_management(landfeatureEOV,"landfeature_lyr_EOV",query)
+      arcpy.CopyFeatures_management("landfeature_lyr_EOV","C:\landlayer_EOV.SHP")
+      EOV = os.path.join("C:\landlayer_EOV.SHP")
       Desc_EOV = arcpy.Describe(EOV)
       Ext = Desc_EOV.extent
    
    if wkid == 32633:
-      arcpy.MakeFeatureLayer_management(kulhatfeatureUTM,"kulhatfeature_lyr_UTM",query)
-      arcpy.CopyFeatures_management("kulhatfeature_lyr_UTM","R:\Kereso_allomanyai\kulhatlayer_UTM.SHP")
-      UTM = os.path.join("R:\Kereso_allomanyai\kulhatlayer_UTM.SHP")
+      arcpy.MakeFeatureLayer_management(landfeatureUTM,"landfeature_lyr_UTM",query)
+      arcpy.CopyFeatures_management("landfeature_lyr_UTM","C:\kulhatlayer_UTM.SHP")
+      UTM = os.path.join("C:\kulhatlayer_UTM.SHP")
       Desc_UTM = arcpy.Describe(UTM)
       Ext = Desc_UTM.extent
 
@@ -65,17 +64,13 @@ for dataset in inputs.split(';'):
       if Ext.disjoint(rasterExt):
          pass
       else: 
-         arcpy.AddMessage(str(pathvalue) + " hozzáadva a réteghez.")
+         arcpy.AddMessage(str(pathvalue) + " added to the layer.")
 		 
          mxd = arcpy.mapping.MapDocument("CURRENT")
          df = arcpy.mapping.ListDataFrames(mxd,"*")[0]
 		 
          newlayer = arcpy.mapping.Layer(pathvalue)
-		 
-         if rasterDesc.bandCount == 3:
-            alaplayer = arcpy.mapping.Layer(r"R:\Kereso_allomanyai\rgb.lyr")
-            arcpy.mapping.UpdateLayer(df,newlayer,alaplayer,True)
-            del alaplayer
+         
          arcpy.mapping.AddLayer(df,newlayer,"TOP")
          arcpy.RefreshTOC()
          del mxd, df, newlayer
