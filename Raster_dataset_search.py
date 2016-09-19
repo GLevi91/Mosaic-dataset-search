2fc1c408349292939220300ed07c5387d1245058
Gombos Levente <glevi91@hotmail.com>
2016-09-19 20:59:46 +0200

    Source code added.
    Completely rewritten code. The script is also located in the toolbox, but it is easier to view and edit if I upload it separately too.

---
+++ Raster_dataset_search.py
@@ -0,0 +1,49 @@
+﻿import os, sys, arcpy
+import arcpy.mapping
+
+reload (sys)
+sys.setdefaultencoding('utf-8')
+
+inputFolder = sys.argv[1]
+inputMosaic = sys.argv[2]
+inputPolygon = sys.argv[3]
+inputField = sys.argv[4]
+inputValue = sys.argv[5]
+
+
+arcpy.env.overwriteOutput = True
+
+
+inputFolderParameters = arcpy.GetParameterAsText(0)
+inputMosaicParameters = arcpy.GetParameterAsText(1)
+
+
+desc = arcpy.Describe(inputPolygon).shapeFieldName
+query = "%s = '%s'" %(inputField, inputValue)
+searchcursor = arcpy.SearchCursor(inputPolygon, query)
+
+
+for folder in inputFolderParameters.split(";"):
+   arcpy.env.workspace = folder
+   
+   for row in searchcursor:
+      rowvalue = row.getValue(desc)
+      polygonextent = rowvalue.extent
+	  
+      for raster in arcpy.ListRasters("*"):
+         rasterdescribe = arcpy.Describe(raster)
+         rasterextent = rasterdescribe.extent
+		 
+         if polygonextent.disjoint(rasterextent):
+            pass
+         else:
+            mxd = arcpy.mapping.MapDocument("CURRENT")
+            df = arcpy.mapping.ListDataFrames(mxd, "*")[0]
+			
+            newlayer = arcpy.mapping.Layer(raster)
+			
+            arcpy.mapping.AddLayer(df,newlayer,"TOP")
+            arcpy.RefreshTOC()
+            del mxd, df, newlayer
+		
+arcpy.RefreshActiveView()
